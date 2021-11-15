871cca26e557201f91840bdbc4402b6c39b4fcf7
Gustavo Pimenta <84296069+GustavoPimenta278@users.noreply.github.com>
2021-11-15 19:08:56 -0300

    Add files via upload

---
+++ JavaScript/Ex014/hours.js
@@ -0,0 +1,23 @@
+
+function preencherSection () {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem')
+
+    var dataAtual = new Date()
+    var hora = dataAtual.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manhã.jpeg'
+        document.body.style.background = '#e2cd9f'
+
+    }   else if (hora >= 12 && hora < 18) {
+        img.src = 'tarde.jpeg'
+        document.body.style.background = '#b9846f'
+
+    }   else {
+        img.src = 'noite.jpeg'
+        document.body.style.background = '#515154'
+    }
+}
